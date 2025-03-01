fba8cfd19446b1a826191f237f88da732333473c
Rut <rutpatel392@gmail.com>
2025-03-01 13:37:31 -0500

    changes

---
+++ rutpatel.io/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to Rut Patel's Website</h1>
+      <p>This is a simple website built with Ruby, JavaScript, jQuery, and HTML!</p>
+      <button id="myButton">Click Me!</button>
+    </div>
+  );
+};
+
+export default App;
